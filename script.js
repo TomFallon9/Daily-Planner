1566db2b5d7b5abc88f5e500c1d7dca54f94e6a4
Tom Fallon <tomfallon9@gmail.com>
2021-01-24 15:06:34 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+   let value;
+   let time;
+ 
+   // Calls moment
+   moment();
+ 
+   // Shows the date
+   let date = moment().format("dddd, MMMM Do");
+   $("#currentDay").text(date);
+ 
+   // Grabs current time
+   let currentTime = moment().hours();
+   
+   // Checks if time-block is past, present, future
+   $(".textarea").each(function (i) {
+     if (currentTime === i + 9) {
+       $(this).addClass("present");
+     } else if (currentTime > i + 9) {
+       $(this).addClass("past");
+     } else if (currentTime < i + 9) {
+       $(this).addClass("future");
+     }
+   });
+ 
+   // Check local storage
+   renderInputs();
+ 
+   function renderInputs() {
+     for (let i = 9; i < 18; i++) {
+       value = localStorage.getItem(i);
+       $("#hour-" + i).val(value);
+     }
+   }
+ 
+   // Stores text input
+   $(".saveBtn").on("click", function () {
+     value = $(this).siblings(".textarea").val().trim();
+     time = $(this).siblings(".hour").attr("id");
+     localStorage.setItem(time, value);
+     renderInputs();
+   });
+ });
